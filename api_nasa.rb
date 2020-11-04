f44201f2f866da203fba30fd34c7760c5e11965e
alfredopl <aalfredoplz@gmail.com>
2020-11-03 23:27:31 -0300

    Method buid_web_page

--- api_nasa.rb
+++ api_nasa.rb
@@ -17,6 +17,31 @@ def request(url , key)
         hash
 end
 
-puts request(url,key)
+def buid_web_page(hash)
 
+    html = "<html>\n"
+    head ="<head>\n"
+    tittle = "<title>Desafio Api Nasa</title>\n"
+    cierre_head ="</head>\n"
+    body = "<body>\n"
+    h1= "<h1>Desafio Api Nasa</h1>"
+    ul = "<ul style='display: flex;flex-wrap: wrap;justify-content: space-evenly;'>\n"
+    li=""
+        hash.each do |k , v|
+            li += "<li style='list-style: none;'><img src='#{v}' style='width:300px;height:300px'></li>\n"
+        end
+    ciere_ul ="</ul\n>"
+    cierre_body ="</body>\n"
+    cierre_html="</html>"
+
+    build = html+head+tittle+cierre_head+body+h1+ul+li+ciere_ul+cierre_body+cierre_html
+
+
+
+    File.write('index.html', build)
+
+end
+
+hash = request(url,key)
+buid_web_page(hash)
 
